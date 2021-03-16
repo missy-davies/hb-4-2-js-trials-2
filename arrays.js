f89b36a8e9600cfc36809b214bfa723374ef2b90
Deborah <deborah.oyt@gmail.com>
2021-03-16 19:54:23 +0000

    Second commit-completed arrays.js
    Co-authored-by: Missy Davies <ms.melissadavies@gmail.com>

--- arrays.js
+++ arrays.js
@@ -10,15 +10,25 @@ function printIndices(items) {
 }
 
 
-printIndices(['apple', 'berry', 'cherry'])
-
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  
+let result = [];
+for(let i=0; i<items.length; i+=1){
+  if (i % 2 === 0) {
+    result.push(items[i]);
+  } 
+} return result;
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
+  const sorted_items = items.sort((a,b) => a-b);
+  const sorted_n_items = sorted_items.slice(0,n);
+  sorted_n_items.reverse();
   // Replace this with your code
+ console.log(sorted_n_items)
 }
+
+
